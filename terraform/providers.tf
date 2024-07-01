91c520ff83759e4a4c6d12a995f6dba98a0a5cc0
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-01 21:27:36 +0100

    Refactor Terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -36,15 +36,3 @@ provider "azurerm" {
 
   features {}
 }
-
-data "azurerm_client_config" "current" {}
-
-data "azuread_client_config" "current" {}
-
-resource "random_id" "environment_id" {
-  byte_length = 6
-}
-
-resource "time_rotating" "rotate" {
-  rotation_days = 30
-}
